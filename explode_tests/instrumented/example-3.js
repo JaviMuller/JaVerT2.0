c7a6231227c94c192d1f722f02115d39188d3f3f
JaviMuller <javier.de.muller@tecnico.ulisboa.pt>
2022-07-13 15:27:24 +0100

    Corrected example 3

--- explode_tests/instrumented/example-3.js
+++ explode_tests/instrumented/example-3.js
@@ -8,7 +8,8 @@ o.w = w;
 function f(o) {
     if (o.z > 0) {
         o.y = "2";
-		var ret = !is_symbolic(o.y) && !is_symbolic(o.w);
+		var out = o.y + o.w;
+		ret = !is_symbolic(out);
 		Assert(ret);
         return eval(o.y + o.w);
     }
