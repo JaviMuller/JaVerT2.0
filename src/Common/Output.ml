f6b6c2f45c32983c93974c0e2c8743399d6b230c
JaviMuller <javier.de.muller@tecnico.ulisboa.pt>
2022-07-14 15:58:12 +0100

    output fully functional

--- src/Common/Output.ml
+++ src/Common/Output.ml
@@ -19,7 +19,7 @@ let file = ref ""
 
 (** Directory to save output files*)
 let dir = "./_out/"
-let create_dir perm = if not (Sys.file_exists dir) then Unix.mkdir dir perm
+let create_dir () : unit = if not (Sys.file_exists dir) then Unix.mkdir dir 0o755
 
 (** Prefix for output files *)
 let output_prefix = "out_"
@@ -33,21 +33,19 @@ let output_extension (lvl: t) : string =
 let filename (lvl: t) : string = 
   output_prefix ^ !file ^ "." ^ (output_extension lvl)
 
-let out_normal = ref (open_out (dir ^ (filename Normal)))
-let out_json = ref (open_out (dir ^ (filename JSON)))
+let out_normal = ref stdout
+let out_json = ref stdout
 
 let wrap_up () : unit = 
   close_out !out_normal;
   close_out !out_json
 
-let json_obj_written = ref false
-
 let open_f() : unit =
-  create_dir 0o640;
+  create_dir();
   if (!enabled && (!mode = 1 || !mode = 2)) then 
-    out_normal := open_out_gen [Open_creat; Open_text; Open_wronly] 0o640 (dir ^ (filename Normal));
+    out_normal := open_out_gen [Open_creat; Open_text; Open_wronly; Open_trunc] 0o640 (dir ^ (filename Normal));
   if (!enabled && (!mode = 0 || !mode = 2)) then
-    out_json := open_out_gen [Open_creat; Open_text; Open_wronly] 0o640 (dir ^ (filename Normal))
+    out_json := open_out_gen [Open_creat; Open_text; Open_wronly; Open_trunc] 0o640 (dir ^ (filename JSON))
 
 
 let write lvl (txt : string lazy_t) : unit =
@@ -57,17 +55,4 @@ let write lvl (txt : string lazy_t) : unit =
     (match lvl with
       | Normal -> if (!mode = 1 || !mode = 2) then output_string !out_normal txt
       | JSON -> if (!mode = 0 || !mode = 2) then 
-        output_string !out_json txt;
-        json_obj_written := true)
-
-let write_json_begin() : unit =
-  if (!enabled && (!mode = 0 || !mode = 2)) then
-    output_string !out_json (Printf.sprintf "[\n")
-
-let write_json_end() : unit =
-  if (!enabled && (!mode = 0 || !mode = 2) && !json_obj_written) then (
-    (* Overwrite last comma *)
-    seek_out !out_json ((pos_out !out_json) - 2);
-    output_string !out_json (Printf.sprintf "\n]\n"))
-  else if (!enabled && (!mode = 0 || !mode = 2)) then
-    output_string !out_json (Printf.sprintf "]\n")
+        output_string !out_json txt)
