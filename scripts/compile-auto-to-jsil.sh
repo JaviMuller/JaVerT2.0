c00b4e69f630585d4d8ed2bb870cdb1334df83fc
JaviMuller <javier.de.muller@tecnico.ulisboa.pt>
2022-09-05 17:05:31 +0100

    Corrected compile-auto-to-jsil.sh

--- scripts/compile-auto-to-jsil.sh
+++ scripts/compile-auto-to-jsil.sh
@@ -7,5 +7,5 @@ number=`find ./explode_tests/instrumented_auto -name 'example-*.js' | wc -l`
 for i in $(seq 0 $(($number - 1)))
 do
 	echo $i
-	js2jsil.native -file explode_tests/instrumented_auto/example-${i}.js -cosette
+	./js2jsil.native -file explode_tests/instrumented_auto/example-${i}.js -cosette
 done
