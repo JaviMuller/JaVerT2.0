21d12bfdb1042506bca6b75e94c00fe03715a60d
JaviMuller <javier.de.muller@tecnico.ulisboa.pt>
2022-09-06 22:29:35 +0100

    Corrected example 15, added review of all tests, corrected scripts

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -7,5 +7,5 @@ number=`find ./explode_tests/instrumented_auto -name 'example-*.jsil' | wc -l`
 for i in $(seq 0 $(($number - 1)))
 do
 	echo $i
-	./cosette.native -file explode_tests/instrumented_auto/example-${i}.jsil -silent -out-mode=2
+	./cosette.native -file explode_tests/instrumented_auto/example-${i}.jsil -silent -out_mode 2
 done
